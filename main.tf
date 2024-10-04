63563f7d1b97fd8f02b2fb4dd8938a2ee38c020c
blank <hayaken3007@i.softbank.jp>
2024-10-05 05:01:33 +0900

    output var

--- main.tf
+++ main.tf
@@ -6,29 +6,42 @@ variable "image_id" {
   # sensitive = true # 任意, これを設定すると、Terraform planやapplyコマンドのアウトプットに値が表示されなくなる
 }
 
+variable "amis" {
+  type = map
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    ap-northeast-1 = "ami-034968955444c1fd9"
+  }
+}
+
 # "aws_instance"というタイプのresourceを定義し、 そのリソースに"web"というローカルネームをつける
 resource "aws_instance" "web" {
   # aws_instance resourceのArguments
-  ami           = var.image_id
+  ami           = lookup(var.amis, var.aws_region)
   instance_type = "t2.micro"
 }
 
+# output blockを使って、"instance_ip_address"というアウトプット変数を定義
+output "instance_ip_address" {
+  value = aws_instance.web.private_ip
+  description = "The private IP address of the main server instance."
+  # sensitive = true # 任意, これを設定すると、Terraform planやapplyコマンドのアウトプットに値が表示されなくなる
+}
 
 
-## 最低限必要なAWS Config (AWS CLIで”aws config”を実行して、Region、Access Keyなどを設定するのと同じ) ##
+## AWS Config ##
 variable "aws_region" {
   default = "ap-northeast-1"
 }
 variable "aws_profile" {
-  type = string
-  default = "terraform"
-  description = "AWS CLI's profile"
+  # type = string
+  default = "aws-demo"
+  # description = "AWS CLI's profile"
 }
 provider "aws" {
-    #　Access keyを使っても酔いが、AWSのProfileの方が便利且つ、Secret Access Keyを間違ってGitにコミットしてしまう恐れがなく安全。
     # access_key = var.aws_access_key
     # secret_key = var.aws_secret_key
-    
     region = var.aws_region
     profile = var.aws_profile
 }

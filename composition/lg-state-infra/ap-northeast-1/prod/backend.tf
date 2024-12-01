753d0d2e3901e4c2d0f5393b027bb5919ea5c25a
omniarchy <k.hayashi@cresplanex.com>
2024-12-02 04:58:52 +0900

    update

--- composition/lg-state-infra/ap-northeast-1/prod/backend.tf
+++ composition/lg-state-infra/ap-northeast-1/prod/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-apne1-lg-state-prod-terraform-backend-5d1790bc"
+    bucket         = "s3-apne1-lg-state-prod-terraform-backend-119eab4d"
     region         = "ap-northeast-1"
-    profile        = "terraform"
     key            = "lg-state-infra/ap-northeast-1/prod/terraform.tfstate"
     dynamodb_table = "dynamo-apne1-lg-state-prod-terraform-state-lock"
     encrypt        = true
